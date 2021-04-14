ffafa0def7a325231357ca071913a90ef0d4748f
Sascha Bast <sash@mischkonsum.org>
2021-04-14 10:39:03 +0200

    init

--- cmdf.py
+++ cmdf.py
@@ -7,17 +7,15 @@ import numpy
 import pandas as pd
 from flask import Flask, request
 
-HISTORY = "/home/my5t3ry/.zsh_history"
-
 app = Flask(__name__)
 
-words = []
-probs = {}
+words =[]
+probs  =[]
 word_freq_dict = None
 Total = None
 
 @app.route('/correct', methods=['POST'])
-def get_suggestions():
+def get_tasks():
   global words
   global probs
   global word_freq_dict
@@ -30,13 +28,12 @@ def get_suggestions():
   similarities = [(Levenshtein.distance(v, cmd)) for v in
                   word_freq_dict.keys()]
   df = pd.DataFrame.from_dict(probs, orient='index').reset_index()
-  df = df.rename(columns={0: 'Prob'})
+  df = df.rename(columns={ 0: 'Prob'})
   df['Similarity'] = similarities
-  df['cmd'] = word_freq_dict.keys()
+  df['cmd'] =  word_freq_dict.keys()
   output = df.sort_values(['Similarity'], ascending=True)
 
-  return "{\"cmd\":\"" + list(output.head(1).to_dict('dict')["cmd"].values())[
-    0] + "\"}"
+  return "{\"cmd\":\"" + list(output.head(1).to_dict('dict')["cmd"].values())[0]+"\"}"
 
 
 def init():
@@ -45,25 +42,34 @@ def init():
   global word_freq_dict
   global Total
   words_str = subprocess.check_output(
-      "echo \"$(cat %s)\"" % HISTORY, shell=True)
+      "echo \"$(cat /home/my5t3ry/.zsh_history)\"", shell=True)
 
   for word in words_str.splitlines():
     try:
       cur = word.decode(encoding="utf-8").split(";")[1]
       words.append(cur)
+      if "sshc" in cur:
+        print(cur)
     except Exception as e:
-      print("shit ['{}']".format(e))
+      print("shit")
 
   V = numpy.array(words)
   unique, counts = numpy.unique(V, return_counts=True)
   dict(zip(unique, counts))
 
+  print(f"The first ten words in the text are: \n{words[0:10]}")
+  print(f"There are {len(unique)} unique words in the vocabulary")
   word_freq_dict = {}
   word_freq_dict = Counter(words)
   print(word_freq_dict.most_common()[0:10])
+  probs = {}
   Total = sum(word_freq_dict.values())
+
 
 
 if __name__ == '__main__':
   init()
   app.run(debug=True, port=7755)
+
+
+
